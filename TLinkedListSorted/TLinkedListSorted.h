260c495f896f51956918fc8f7a565b70d420312b
NicNaves <72363369+NicNaves@users.noreply.github.com>
2020-10-27 00:25:33 -0300

    Sorted

---
+++ TLinkedListSorted/TLinkedListSorted.h
@@ -0,0 +1,40 @@
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
+#define SORTED -5
+
+
+
+struct student{
+    int id;
+    char name[30];
+    float g1,g2,g3;
+};
+
+typedef struct TLinkedList TLinkedList;
+
+TLinkedList* list_create(int x);
+int list_free(TLinkedList *li);
+
+int list_push_front(TLinkedList *li, struct student sl);
+int list_push_back(TLinkedList *li, struct student sl);
+int list_insert(TLinkedList *li, int pos, struct student sl);
+
+int list_insert_sorted(TLinkedList *li, struct student sl);
+
+int list_size(TLinkedList *li);
+
+
+int list_pop_front(TLinkedList *li);
+int list_pop_back(TLinkedList *li);
+int list_erase(TLinkedList *li, int pos);
+
+int list_find_pos(TLinkedList *li, int pos, struct student *sl);
+int list_find_mat(TLinkedList *li, int nmat, struct student *sl);
+int list_front(TLinkedList *li, struct student *sl);
+int list_back(TLinkedList *li, struct student *sl);
+int list_get_pos(TLinkedList *li, int id, int *pos);
+
+int list_print(TLinkedList *li);
---
+++ TLinkedListSorted/main.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "TLinkedListSorted.h"
+
+int main(){
+  struct student s[6] = 
+  {{2,"Nícolas",9.5,7.8,8.5},
+  {4,"Gustavo",7.5,8.7,6.8},
+  {5,"Lucas",9.7,6.7,8.4},
+  {1,"Diego",9.5,5.8,10.0},
+  {6, "Paulo", 7.4,8.5,8.8},
+  {3, "Maycon", 7.8,9.5,8.9}};
+
+  struct student sl[3] =
+  {{1,"Douglas",9.5,5.8,10.0},
+  {6, "Pablo", 7.4,8.5,8.8},
+  {3, "Bruno", 7.8,9.5,8.9}};
+
+  struct student aux;
+  
+  TLinkedList *list;
+  list = list_create(1);
+  int ret;
+  int i = 0;
+  int pos=0;
+  
+  printf("\nIniciando com 6 alunos em ordem \n\n");
+  while (i<6){
+    if((ret = list_insert_sorted(list, s[i])) == SUCCESS)
+      i++;
+    else if (ret == INVALID_NULL_POINTER)
+      printf("\nPonteiro inválido.");
+    else if (ret == OUT_OF_MEMORY)
+      printf("\nSem Memória.");
+    else if (ret == SORTED)
+      printf("\nLista nao Ordenada.");
+  }
+  list_print(list);
+
+  printf("\n\n Adicionando no inicio \n\n");
+  if((ret = list_push_front(list,  sl[0])) == SUCCESS)
+    list_print(list);
+  else if (ret == SORTED)
+      printf("\nLista não Ordenada.");
+  else
+    printf("\nPonteiro inválido.");
+
+  
+  printf("\n\n Adicionando no final \n\n");
+  if((ret = list_push_back(list, sl[1])) == SUCCESS)
+    list_print(list);
+  else if (ret == SORTED)
+      printf("\nLista não Ordenada.");
+  else
+    printf("\nPonteiro inválido.");
+  
+  printf("\n\n Adicionando na posição 3 \n\n");
+  if((ret = list_insert(list, 3, sl[2])) == SUCCESS)
+    list_print(list);
+  else if (ret == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+  else if (ret == OUT_OF_MEMORY)
+    printf("\nSem Memória.");
+  else
+      printf("\nLista não Ordenada.");
+  
+
+  printf("\n\n Consulta quem está na posição 5 \n\n");
+  if((ret = list_find_pos(list, 5, &aux)) == SUCCESS)
+    printf("Id: %d \nNome: %s\nNotas: ( %.2f, %.2f, %.2f )\n",aux.id, aux.name, aux.g1, aux.g2,aux.g3);
+  else if(ret == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+  else if (ret == ELEM_NOT_FOUND)
+    printf("\nElemento não encontrado.");
+  
+  printf("\n\n Consulta quem possui matrícula 4  \n\n");
+  if((ret = list_find_mat(list,  4, &aux)) == SUCCESS)
+    printf("Id: %d \nNome: %s\nNotas: ( %.2f, %.2f, %.2f )\n",aux.id, aux.name, aux.g1, aux.g2,aux.g3);
+  else if(ret == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+  else if (ret == ELEM_NOT_FOUND)
+    printf("\nElemento não encontrado.");
+  
+  printf("\n\n Consulta quem é o primeiro \n\n");
+  if((ret = list_front(list, &aux)) == SUCCESS)
+    printf("Id: %d \nNome: %s\nNotas: ( %.2f, %.2f, %.2f )\n",aux.id, aux.name, aux.g1, aux.g2,aux.g3);
+  else
+    printf("\nPonteiro inválido.");
+  
+  printf("\n\n Consulta quem é o último \n\n");
+  if((ret = list_back(list, &aux)) == SUCCESS)
+    printf("Id: %d \nNome: %s\nNotas: ( %.2f, %.2f, %.2f )\n",aux.id, aux.name, aux.g1, aux.g2,aux.g3);
+  else
+    printf("\nPonteiro inválido.");
+  
+  printf("\n\n Consulta qual posição a matrícula 4 está \n\n");
+  if((ret = list_get_pos(list, 4,  &pos)) == SUCCESS)
+    printf("Posição: %d", pos);
+  else if(ret == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+  else if (ret == ELEM_NOT_FOUND)
+    printf("\nElemento não encontrado.");
+
+
+  printf("\n\n Remove no inicio \n\n");
+  if((ret = list_pop_front(list)) == SUCCESS)
+    list_print(list);
+  else
+    printf("\nPonteiro inválido.");
+  
+  printf("\n\n Remove no final \n\n");
+  if((ret = list_pop_back(list)) == SUCCESS)
+    list_print(list);
+  else
+    printf("\nPonteiro inválido.");
+  
+  printf("\n\n Remove a posição 2\n\n");
+  if((ret = list_erase(list, 1)) == SUCCESS)
+    list_print(list);
+  else if(ret == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+  else if (ret == OUT_OF_RANGE)
+    printf("\nElemento fora de alcance.");
+
+  
+  printf("\n\n Tamanho da Lista: %d\n\n", list_size(list));
+
+  if((ret = list_free(list)) == INVALID_NULL_POINTER)
+    printf("\nPonteiro inválido.");
+
+  return 0;
+}
+
